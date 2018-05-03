122e0b8f2d61e5c4ea50fed6f5c1529165aff756
Stefan Waldl <waldl.stefan@gmail.com>
2018-05-03 08:08:15 +0200

    finished

--- src/main/java/GermanStyleDateChecker.java
+++ src/main/java/GermanStyleDateChecker.java
@@ -2,7 +2,11 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GermanStyleDateChecker {
+
     public static Matcher getMatcher(String dateString) {
-        return null;
+        return Pattern
+                .compile("^(([0-1]?[0-9])|(30|31))[.\\- ]((1[0-2])|(0?[0-9]))[.\\- ]((((19)|(2[0-9]))[0-9][0-9])|([1-9][0-9]))$")
+                .matcher(dateString);
     }
+
 }
